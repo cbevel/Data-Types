9b2ba7a52acb48282158af1371cf59b1528289d6
cbevel <chelsey.bevel@gmail.com>
2020-03-02 02:13:41 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let shuttleName = 'Determination';
+let shuttleSpeedKm = 17500;
+let distanceToMarsKm = 225000000;
+let distanceToMoonKM = 384400;
+let milesPerKilometer = 0.621;
+
+let milesToMoon = 238712.4;
+let hoursToMoon = 13.64070857142857;
+
+
+console.log(distanceToMoonKM * milesPerKilometer);
+console.log(milesToMoon / shuttleSpeedKm);
+console.log(hoursToMoon / 24);
